93bb0cc100c4008cb260d5c4f88ef4afc8db83eb
Julie Pagano <julie.pagano@gmail.com>
2012-11-14 22:33:45 -0500

    Improve box-shadow examples

--- js/slides.js
+++ js/slides.js
@@ -34,4 +34,50 @@ $(document).ready(function() {
     parent.find("tbody tr").css("background-color", "");
     rows.css("background-color", "red");
   });
+
+  $(".box-shadow .example1").on("change", "input", function() {
+    var parent    = $(this).closest("section"),
+        inputs    = parent.find("input"),
+        index     = inputs.index($(this)),
+        new_value = $(this).val()
+        setting   = "";
+
+    parent.find("pre .number").eq(index).text(new_value);
+
+    inputs.each(function() {
+      setting += $(this).val() + "px ";
+    });
+    setting += " red";
+
+    parent.find("div").css("box-shadow", setting);
+  });
+
+  $(".box-shadow .example2").on("change", "input", function() {
+    var parent    = $(this).closest("section"),
+        inputs    = parent.find("input[type='range']"),
+        index     = inputs.index($(this)),
+        new_value = $(this).val(),
+        setting   = "";
+
+    if(parent.find("input[type='checkbox']:checked").length) {
+      setting += "inset ";
+
+      parent.find("pre span.value").prepend("<span class='temp'> inset</span>");
+    }
+    else {
+      parent.find("pre span.value span.temp").remove();
+    }
+
+    setting += "0 0 ";
+
+    inputs.each(function(index) {
+      var value = $(this).val();
+      setting += value + "px ";
+      parent.find("pre .number").eq(index + 2).text(value);
+    });
+    setting += " red";
+    console.log(setting);
+
+    parent.find("div").css("box-shadow", setting);
+  });
 });
